bd165d194c030d196566532f34ba04a919211e5f
Ng.V.Hoàng <134377540+xasduong@users.noreply.github.com>
2024-02-19 14:28:54 +0700

    Them chuc nang server truyen tranh

--- js/index.js
+++ js/index.js
@@ -6,15 +6,18 @@ let title = z('title');
 let boxImg = z('#web-img');
 let boxNav = z('#box-name_list');
 let currentComic = z('.current-comic');
-let isServer = 1;
 let webItem = z('#web-item')
 let getRandom = Math.floor(Math.random() * images.length);
 let boxComic = z('#box-comic');
+let overLay = z('#overlay');
+let boxSet = z('#box-set');
+let boxSetWrap = z('.box-set_wrap');
+let wrapCloseBtn = z('.set_wrap-close');
 (function startWeb() {
 	handleImages(getRandom, boxImg);
 	let arrayImages = images.sort((a, b) => b.lengthImg - a.lengthImg)
 	webItem.innerHTML = arrayImages.map(renderItemComic).join('');
-	handleWebItem();
+	handleServer(); handleWebItem(getRandom); handleBoxSetWrap();
 	webItem.onclick = () => {
 		webItem.classList.add('close');
 	}
@@ -22,7 +25,46 @@ let boxComic = z('#box-comic');
 		webItem.classList.remove('close');
 	}
 })();
-function handleWebItem() {
+//	Xu kien cuon chuot 
+window.addEventListener('scroll', () => {
+	var x = pageYOffset;
+})
+function handleBoxSetWrap() {
+	wrapCloseBtn.onclick = () => {
+		overLay.classList.add('close');
+	}
+	boxSet.onclick = () => {
+		boxSetWrap.style.top = '160%';
+		boxSetWrap.style.opacity = '1';
+		overLay.classList.remove('close');
+		boxSet.style.background = 'rgb(27, 44, 70)';
+	}
+	overLay.onclick = closeBoxSetWrap;
+	function closeBoxSetWrap(){
+		overLay.classList.add('close');
+		boxSetWrap.style.top = '-200px';
+		boxSetWrap.style.opacity = '0';
+		boxSet.style.background = 'transparent';
+	}
+} ;
+function handleServer() {
+	let serverBtns = zz('.set_wrap-server > span');
+	(() => {
+		let isServer = localStorage.getItem('isServer');
+		if (isServer != null) {
+			z('.set_wrap-server > span.choose').classList.remove('choose');
+			serverBtns[isServer].classList.add('choose');
+		}
+	}) ();
+	serverBtns.forEach((btn, index) => {
+		btn.onclick = () => {
+			z('.set_wrap-server > span.choose').classList.remove('choose');
+			btn.classList.add('choose');
+			localStorage.setItem('isServer', index);
+		}
+	})
+} 
+function handleWebItem(getRandom) {
 	let comicInfor = zz('.web-item_infor');
 	comicInfor[getRandom].classList.add('item_choose')
 	comicInfor.forEach((item, index) => {
@@ -70,18 +112,30 @@ function handleImages(index, boxImg){
 		for (let i = 1; i < images[index].lengthImg; i++) {
 			let img = document.createElement('img');
 			img.setAttribute('id', "numImg-" + i);
-			switch(isServer) {
-				case 0: {
-					img.src = "https://i.postimg.cc/" + getListImg[i].nameUrl + "/"+ getNameComic 
-						+ "-" + getListImg[i].numImg + '.jpg';
-					break;
-				}
-				case 1: {
-					img.src = "img/" + images[index].nameComic + "/"+ images[index].nameComic+ ' ('+ i +').jpg'
-					break;
-				}
+			let isServer = +localStorage.getItem('isServer');
+			if( isServer == null) {
+				isServer = 0;
+				img.src = "img/" + images[index].nameComic + "/"+ images[index].nameComic+ ' ('+ i +').jpg'
+				boxImg.appendChild(img);
 			}
-			boxImg.appendChild(img);
+			else {
+				switch(isServer) {
+					case 0: {
+						img.src = "img/" + images[index].nameComic + "/"+ images[index].nameComic+ ' ('+ i +').jpg'
+						break;
+					}
+					case 1: {
+						img.src = "https://i.postimg.cc/" + getListImg[i].nameUrl + "/"+ getNameComic 
+							+ "-" + getListImg[i].numImg + '.jpg';
+						break;
+					}
+					case 2: {
+						console.log('Chưa có dữ liệu hiển thị');
+						break;
+					}
+				}
+				boxImg.appendChild(img);
+			}
 		}
 	}
 }
